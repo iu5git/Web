1a4961d98846ddf745ad0fbad40bc9a8fb785fd6
puzzzik <vtazenkovv@yandex.ru>
2022-10-04 22:20:22 +0300

    lab3 done

--- lab1/lab1/urls.py
+++ lab1/lab1/urls.py
@@ -2,18 +2,21 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from tobacco_app import views
+from tobacco_app.viewSets import ProductViewSet, CustomerViewSet, OrderViewSet
 
 
 
 router = routers.DefaultRouter()
-router.register(r'tobacco', views.TobaccoViewSet)
+router.register(r'products', ProductViewSet)
+router.register(r'customer', CustomerViewSet)
+router.register(r'order', OrderViewSet)
 
 urlpatterns = [
     path('hello/', views.hello),
-    path('products', views.ProductList),
+    path('productList', views.ProductList),
     path('product/<int:id>/', views.get_product, name='product_url'),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
-    path('tobacco-app', include('tobacco_app.urls')),
+    # path('tobacco-app', include('tobacco_app.urls')),
     path('', include(router.urls)),
     ]
---
+++ lab1/tobacco_app/viewSets.py
@@ -0,0 +1,105 @@
+from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
+from rest_framework import  status
+from rest_framework.response import Response
+from tobacco_app.models import *
+from tobacco_app.serializers import *
+
+class ProductViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint, который позволяет просматривать и редактировать акции компаний
+    """
+    # queryset всех пользователей для фильтрации по дате последнего изменения
+    queryset = Product.objects.all().order_by('pk')
+    serializer_class = ProductSerializer  # Сериализатор для модели
+
+    def list(self, request):
+        queryset = Product.objects.all()
+        serializer = ProductSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Product.objects.all()
+        user = get_object_or_404(queryset, pk=pk)
+        serializer = ProductSerializer(user)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        try: 
+            product = Product.objects.get(pk=pk) 
+        except Product.DoesNotExist: 
+            return Response({'message': 'The product does not exist'}, status=status.HTTP_404_NOT_FOUND) 
+        serializer = ProductSerializer(product, data=request.data) 
+        if serializer.is_valid(): 
+            serializer.save() 
+            return Response(serializer.data) 
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
+    
+    def delete(self, request, pk=None):
+        try:
+            product = Product.objects.get(pk=pk)
+        except Product.DoesNotExist: 
+            return Response({'message': 'The product does not exist'}, status=status.HTTP_404_NOT_FOUND) 
+        product.delete()
+        return Response({'message': ' Successfully deleted'}, status=status.HTTP_200_OK)
+
+class CustomerViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint, который позволяет просматривать и редактировать акции компаний
+    """
+    # queryset всех пользователей для фильтрации по дате последнего изменения
+    queryset = Customer.objects.all().order_by('pk')
+    serializer_class = CustomerSerializer  # Сериализатор для модели
+
+    def list(self, request):
+        queryset = Customer.objects.all()
+        serializer = CustomerSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Customer.objects.all()
+        user = get_object_or_404(queryset, pk=pk)
+        serializer = CustomerSerializer(user)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        try: 
+            customer = Customer.objects.get(pk=pk) 
+        except Customer.DoesNotExist: 
+            return Response({'message': 'The customer does not exist'}, status=status.HTTP_404_NOT_FOUND) 
+        serializer = CustomerSerializer(customer, data=request.data) 
+        if serializer.is_valid(): 
+            serializer.save() 
+            return Response(serializer.data) 
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint, который позволяет просматривать и редактировать акции компаний
+    """
+    # queryset всех пользователей для фильтрации по дате последнего изменения
+    queryset = Order.objects.all().order_by('pk')
+    serializer_class = OrderSerializer  # Сериализатор для модели
+
+    def list(self, request):
+        queryset = Order.objects.all()
+        serializer = OrderSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Order.objects.all()
+        user = get_object_or_404(queryset, pk=pk)
+        serializer = OrderSerializer(user)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        try: 
+            order = Order.objects.get(pk=pk) 
+        except Order.DoesNotExist: 
+            return Response({'message': 'The order does not exist'}, status=status.HTTP_404_NOT_FOUND) 
+        serializer = OrderSerializer(order, data=request.data) 
+        if serializer.is_valid(): 
+            serializer.save() 
+            return Response(serializer.data) 
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
