ee151811bf80da3673a9520fd9238706dee1db17
puzzzik <vtazenkovv@yandex.ru>
2022-12-01 18:27:31 +0300

    deleted venv

--- lab1/.venv/lib/python3.10/site-packages/httpie/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-HTTPie: modern, user-friendly command-line HTTP client for the API era.
-
-"""
-
-__version__ = '3.2.1'
-__date__ = '2022-05-06'
-__author__ = 'Jakub Roztocil'
-__licence__ = 'BSD'
